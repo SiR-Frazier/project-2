a10d8b480228a1d4a0dcdd521c984b94d98efbf4
Bryce Frazier <bx.frazier@gmail.com>
2018-01-12 10:15:03 -0800

    buttons are functional

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@
 $(document).ready(function() {
   $("#preference").submit(function(event) {
     var preference = $("input:radio[name=type]:checked").val();
-    console.log(preference);
-    if ("#preference" === "front") {
-      $(".front-end").show();
+
+    if (preference === "front") {
+      $(".front-end").show("front");
       $(".back-end").hide();
     } else {
       $(".back-end").show();
